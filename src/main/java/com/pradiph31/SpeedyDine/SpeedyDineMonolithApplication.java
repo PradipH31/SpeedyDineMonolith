b3be5abf5187d07a795fefeaea7db303b39e98ea
PradipH31 <pradip.hamal31@gmail.com>
2023-06-25 19:39:35 -0500

    Initialized Spring Boot repository
    Added reactive package dependencies
    Added GraphQl package dependency

---
+++ src/main/java/com/pradiph31/SpeedyDine/SpeedyDineMonolithApplication.java
@@ -0,0 +1,13 @@
+package com.pradiph31.SpeedyDine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedyDineMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedyDineMonolithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pradiph31/SpeedyDine/SpeedyDineMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pradiph31.SpeedyDine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedyDineMonolithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
