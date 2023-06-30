066fa871d06d58ab129a39c499c5c5a3966cab1f
PradipH31 <pradip.hamal31@gmail.com>
2023-06-30 11:34:49 -0500

    Added material navigation component

---
+++ dine-client/projects/speedy-dine-app/src/app/dining-navigation/dining-navigation.component.ts
@@ -0,0 +1,19 @@
+import { Component, inject } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-dining-navigation',
+  templateUrl: './dining-navigation.component.html',
+  styleUrls: ['./dining-navigation.component.css']
+})
+export class DiningNavigationComponent {
+  private breakpointObserver = inject(BreakpointObserver);
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+}
