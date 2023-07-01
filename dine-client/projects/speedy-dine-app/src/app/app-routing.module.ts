8407f9e7b2e0a660459e9450b1c7adcbbf7c9673
PradipH31 <pradip.hamal31@gmail.com>
2023-07-01 16:29:36 -0500

    Added routes

--- dine-client/projects/speedy-dine-app/src/app/app-routing.module.ts
+++ dine-client/projects/speedy-dine-app/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroComponent } from './components/home/hero/hero.component';
+import { DiningDashboardComponent } from './components/home/dining-dashboard/dining-dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HeroComponent },
+  { path: 'profile', component: DiningDashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
